1a4ef55d6112df6cca704a54aa61adadb6083054
Sambit Sargam Ekalabya <sambitsargam2003@gmail.com>
2023-08-29 00:44:18 +0530

    Adding Avalanche Contract

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const AvaAddress = "0x2CA89938facA177C6e8d9c55804DCc0a93aF5FDE";
+export const AvaAddress = "0xE01eb816302dBfCa0b3f5624e8737DDa022fD6D6";
+export const ArbAddress = "0x8628d91E8311B4856ae7d3E317a0E8fFf48E0f98";
